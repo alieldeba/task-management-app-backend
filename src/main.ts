214390f339be90bb2c7579f7e57a93ab1300709e
alieldeba <alieldeba375@gmail.com>
2024-08-01 08:29:49 +0300

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,7 @@ dotenv.config();
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
 
-    // To fix cors error
-    app.use((req: any, res: any, next: any) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-        next();
-    });
-
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(3000);
+    await app.listen(8000);
 }
 bootstrap();
