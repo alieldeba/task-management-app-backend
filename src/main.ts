b87f939707e0763532d9f4e1d06a38f6c1ec5914
alieldeba <alieldeba375@gmail.com>
2024-08-01 08:50:38 +0300

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,19 @@ import helmet from 'helmet';
 dotenv.config();
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule);
 
-    app.use(helmet());
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+        next();
+    });
+
+    app.enableCors({
+        allowedHeaders: '*',
+        origin: '*',
+    });
 
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(8000);
