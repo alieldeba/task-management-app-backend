c0bb9c3f2e60d85c350d71d600c396209568ed25
alieldeba <alieldeba375@gmail.com>
2024-08-01 08:46:50 +0300

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 // Load environment variables
 dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+
+    app.use(helmet());
 
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(8000);
